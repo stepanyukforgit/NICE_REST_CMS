fd5283d027d74b4be9c9969afb53dceec0b12164
Aleksey Stepanyuk <send.stepanyuk@gmail.com>
2017-12-13 15:33:24 +0200

    Attribut entity refactoring

--- src/main/java/aleksey/stepanyuk/domain/entity/Attribute.java
+++ src/main/java/aleksey/stepanyuk/domain/entity/Attribute.java
@@ -8,27 +8,24 @@ import java.util.Locale;
 import java.util.Map;
 
 @Entity @Table(name = "attribute")
-@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode(of = "id")
-public class Attribute implements Serializable{
+@Getter @Setter @NoArgsConstructor @ToString @EqualsAndHashCode(of = "name")
+public class Attribute implements Serializable {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ElementCollection
-    @MapKeyColumn(name = "language", columnDefinition = "VARCHAR(4)")
+    @MapKeyColumn(name = "language", columnDefinition = "VARCHAR(2)")
     @Column(name = "name", columnDefinition = "VARCHAR(50)")
     @CollectionTable(name = "attribute_names", joinColumns = {
             @JoinColumn(name = "attribute_id", referencedColumnName = "id")})
     private Map<Locale, String> name;
 
     @ElementCollection
-    @MapKeyColumn(name = "language", columnDefinition = "VARCHAR(4)")
-    @Column(name = "value", columnDefinition = "VARCHAR(100)")
+    @MapKeyColumn(name = "language", columnDefinition = "VARCHAR(2)")
+    @Column(name = "value", columnDefinition = "VARCHAR(50)")
     @CollectionTable(name = "attribute_values", joinColumns = {
             @JoinColumn(name = "attribute_id", referencedColumnName = "id")})
     private Map<Locale, String> value;
-
-    @ManyToOne
-    @JoinColumn(name = "product_proposal_id")
-    private ProductProposal productProposal;
 }
